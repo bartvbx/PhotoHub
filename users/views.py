42d096998b63868a84c68e6cc6a15dafc45fa00b
Bart <bartoszty@gmail.com>
2022-07-31 11:26:47 +0200

    Add user registration
    - Add users app in settings.py
    - Create UserRegistrationForm
    - Create register view
    - Link register view to 'register/' url
    - Create register.html template

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import UserRegistrationForm
 
-# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account successfully created for {username}!')
+    else:
+        form = UserRegistrationForm()
+
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
