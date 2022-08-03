194b3345ca73d6f075d9bc60802343308024f1e3
Bart <bartoszty@gmail.com>
2022-08-03 23:46:49 +0200

    Add bootstrap styles
    - Add bootstrap styles for base.html and navbar.html templates
    - Install crispy-forms with bootstrap5 template pack
    - Add crispy to all existing forms

--- photohub/settings.py
+++ photohub/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'photos.apps.PhotosConfig',
+    "crispy_forms",
+    "crispy_bootstrap5",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
