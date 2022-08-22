244429fc10d10d68fa54e78363d17606bd4f8d29
Bart <bartoszty@gmail.com>
2022-08-22 21:24:42 +0200

    Develop frontend: base, photo_list
    - Create a static directory with styles.css and main.js
    - Modify the structure of base.html
    - Delete unnecessary closing div from navbar.html
    - Add footer.html
    - Add styles for photo_list.html
    - Modify PhotoFilter, so it generates filtering fields with CSS class

--- photohub/settings.py
+++ photohub/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
--- photos/filters.py
+++ photos/filters.py
@@ -2,15 +2,22 @@ from django.db.models import Q
 import django_filters
 from .models import Photo, Category
 from django.contrib.auth.models import User
+from django.forms.widgets import TextInput
 
 class PhotoFilter(django_filters.FilterSet):
-    category = django_filters.ModelChoiceFilter(queryset=Category.objects.all(), method='category_filter', label='Select category')
-    author = django_filters.ModelChoiceFilter(queryset=User.objects.all(), method='author_filter', label='Select author')
-    q = django_filters.CharFilter(method='keyword_filter', label="Search by keywords")
+    category = django_filters.ModelChoiceFilter(queryset=Category.objects.all(), method='category_filter', empty_label="All categories")
+    author = django_filters.ModelChoiceFilter(queryset=User.objects.all(), method='author_filter', empty_label="All users")
+    q = django_filters.CharFilter(method='keyword_filter', widget=TextInput(attrs={'placeholder': 'Search by keywords'}))
 
     class Meta:
         model = Photo
         fields = ['category', 'author', 'q']
+
+    def __init__(self, data=None, queryset=None, *, request=None, prefix=None):
+        super(PhotoFilter, self).__init__(data=data, queryset=queryset, request=request, prefix=prefix)
+        self.filters['category'].field.widget.attrs.update({'class': 'photo-search-form-input'})
+        self.filters['author'].field.widget.attrs.update({'class': 'photo-search-form-input'})
+        self.filters['q'].field.widget.attrs.update({'class': 'photo-search-form-input'})
     
     def category_filter(self, queryset, name, value):
         return queryset.filter(category__name__exact=value)
